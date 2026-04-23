77ab69403476c85004dece122bc755b192be000d
jonas25-IT <jonastuyisenge01@gmail.com>
2026-04-23 11:48:10 +0200

    Fix ESLint configuration error in CI/CD pipeline
    - Revert ESLint config to old format compatible with ESLint 8
    - Remove incompatible @eslint/js and globals dependencies
    - Remove eslint-plugin-react-refresh dependency
    - Use traditional module.exports instead of new flat config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.recommended, // Updated to remove 'flat'
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-])
+  rules: {
+    'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+};
